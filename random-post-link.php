a8a1e69fd72f10bd41d747038ddfd1cd3a0a81d9
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-04 09:25:15 +0000

    adding version 1.0
    git-svn-id: http://plugins.svn.wordpress.org/random-post-link/trunk@170285 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ random-post-link.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: Random Post Link
+Description: Generates a link that takes a user to a random post, but never the same as before.
+Version: 1.0
+Author: scribu
+Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/wordpress/random-post-link
+*/
+
+function random_post_link($text = 'Random Post') {
+	printf('<a href="%s">%s</a>', get_random_post_url(), $text);
+}
+
+function get_random_post_url()
+{
+	return trailingslashit(get_bloginfo('url')) . '?' . Random_Post_Link::query_var;
+}
+
+
+Random_Post_Link::init();
+
+class Random_Post_Link
+{
+	const query_var = 'random';
+	const name = 'wp_random_posts';
+
+	function init()
+	{
+		add_action('init', array(__CLASS__, 'jump'));
+
+		// Fire just after post selection
+		add_action('wp', array(__CLASS__, 'manage_cookie'));
+	}
+
+	// Jump to a random post
+	function jump()
+	{
+		if ( ! isset($_GET[self::query_var]) )
+			return;
+
+		$args = apply_filters('random_post_args', array(
+			'post__not_in' => self::read_cookie(),
+		));
+
+		$args = array_merge($args, array(
+			'orderby' => 'rand',
+			'showposts' => 1,
+		);
+
+		$posts = get_posts($args);
+
+		$id = $posts[0]->ID;
+
+		wp_redirect(get_permalink($id));
+		die;
+	}
+
+	// Collect post ids that the user has already seen
+	function manage_cookie()
+	{
+		if ( ! is_single() )
+			return;
+
+		$ids = self::read_cookie();
+		$id = $GLOBALS['posts'][0]->ID;
+
+		if ( count($ids) > 200 )
+			$ids = array($id);
+		elseif ( ! in_array($id, $ids) )
+			$ids[] = $id;
+
+		self::update_cookie($ids);
+	}
+
+	private function read_cookie()
+	{
+		return explode(' ', $_COOKIE[self::name]);
+	}
+
+	private function update_cookie($ids)
+	{
+		setcookie(self::name, trim(implode(' ', $ids)), 0, '/');
+	}
+}
+
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Random Post Link ===
+Contributors: scribu
+Donate link: http://scribu.net/paypal
+Tags: button, redirect
+Requires at least: 2.8
+Tested up to: 2.9-rare
+Stable tag: trunk
+
+Generates a link that takes a visitor to a random post, but never the same as before.
+
+== Description ==
+
+Unlike the [Random Redirect](http://wordpress.org/extend/plugins/random-redirect/) plugin, this plugin ensures that a user will not see the same post twice.
+
+It does this by keeping a list of single posts that the user has seen and then checking that list when redirecting.
+
+= Usage =
+
+Just put this line anywhere in your theme where you want the link to appear.
+
+`<?php random_post_link(); ?>`
+
+You can specify the text you want on the link like so:
+
+`<?php random_post_link('Feeling lucky'); ?>`
+
+== Installation ==
+
+You can either install it automatically from the WordPress admin, or do it manually:
+
+1. Unzip "random-post-link" archive and put the folder into your plugins folder (/wp-content/plugins/).
+1. Activate the plugin from the Plugins menu.
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+* [more info](http://scribu.net/wordpress/random-post-link/rpl-1-0.html)
