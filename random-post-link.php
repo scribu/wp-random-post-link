f925ba5d5e971f80bb52891c306d724e9d6fba63
scribu <mail@scribu.net>
2012-04-30 18:57:53 +0300

    replace copyright notice with license headers

--- random-post-link.php
+++ random-post-link.php
@@ -6,22 +6,8 @@ Version: 1.0.1
 Author: scribu
 Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/random-post-link
-
-
-Copyright (C) 2010 Cristi Burcă (mail@scribu.net)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 function random_post_link($text = 'Random Post') {
